2b9ac70ee3062cc90c6e2c3a4f6643e14c4e56da
@Chinnannavar <yashchinnannavar18@gmail.com.>
2025-02-10 11:10:33 +0530

    index

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-app.listen(3000, () => {
-  console.log('Server listening on "http://localhost:3000"');
-});
+// app.listen(3000, () => {
+//   console.log('Server listening on "http://localhost:3000"');
+// });
